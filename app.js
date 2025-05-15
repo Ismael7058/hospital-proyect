08a44f91088aacbc329687d007119c1210f5a338
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-05-14 23:13:07 -0300

    Agregar vista de error 404 y manejo en app.js
    - Se crea la vista error.pug para páginas no encontradas
    - Se incorpora una imagen ilustrativa en la vista
    - Se creo un styleError de estilo personalizado para error.pug
    - Se agrega middleware en app.js para capturar errores 404 y renderizar la vista personalizada

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 const homeRouter = require("./router/homeRouter");
 const medicoRouter = require('./router/medicoRouter');
 app.use('/', homeRouter);
 app.use('/', medicoRouter);
 
-app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "view"));
+app.use((req, res)=>{
+  res.status(404).render('error')
+});
+
 
 
 app.set("port", process.env.PORT || 3030);
