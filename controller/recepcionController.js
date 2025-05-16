9f1492e0b72d60aadd2c28ae22e413b51932baac
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-05-16 18:10:41 -0300

    Agregar vista registrar de enfermero con su router y controller
    - Se crea la vista registrar.pug para el rol de enfermero
    - Se configura el router de enfermero
    - Se implementa el controller para renderizar la vista correctamente

--- controller/recepcionController.js
+++ controller/recepcionController.js
@@ -1,3 +1,38 @@
 exports.renderHome = (req, res) => {
   res.render("recepcion/home");
 };
+
+// Buscar Paciente
+exports.renderBuscarPaciente = (req, res) => {
+  res.render("recepcion/buscar");
+};
+// Buscar Paciente con filtros
+exports.buscarPacienteFiltrado = (req, res) => {
+  const { dni, nombre, apellido } = req.body;
+  // lógica de búsqueda
+  res.render("recepcion/buscarPaciente", { pacientes: resultado });
+};
+
+
+// Registrar Paciente Vista
+exports.renderRegistrarPaciente = (req, res) => {
+  res.render("recepcion/registrar");
+};
+// Registrar Paciente
+exports.crearPaciente = (req, res) => {
+  const nuevoPaciente = req.body;
+  // lógica para guardar
+  res.redirect("/recepcion");
+};
+
+
+// Internar Paciente Vista
+exports.renderInternarPaciente = (req, res) => {
+  res.render("recepcion/internarPaciente");
+};
+// Internar Paciente
+exports.internarPaciente = (req, res) => {
+  const datos = req.body;
+  // lógica para internar
+  res.redirect("/recepcion");
+};
--- router/recepcionRouter.js
+++ router/recepcionRouter.js
@@ -4,5 +4,10 @@ const recepcionController = require('../controller/recepcionController');
 
 router.get('/', recepcionController.renderHome);
 router.get('/buscar', recepcionController.renderBuscarPaciente);
+router.post('/buscar', recepcionController.buscarPacienteFiltrado);
+router.get('/registrar', recepcionController.renderRegistrarPaciente);
+router.post('/registrar', recepcionController.crearPaciente);
+router.get('/internar', recepcionController.renderInternarPaciente);
+router.post('/internar', recepcionController.internarPaciente);
 
 module.exports = router;
