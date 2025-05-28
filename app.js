34b16462b893a25d9aaa856c0ff1d852ba4126e1
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-05-27 21:36:28 -0300

    Refactoriza app.js y server.js para cargar configuración desde .env + agrega .env.sample

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
+require('dotenv').config();
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
+
+// Sequelize: conexión
+const sequelize = require('./model/db');
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "view"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 const homeRouter = require("./router/homeRouter");
 const medicoRouter = require('./router/medicoRouter');
-const recepcionRouter = require('./router/recepcionRouter')
-const enfermeroRouter = require('./router/enfermeroRouter')
+const recepcionRouter = require('./router/recepcionRouter');
+const enfermeroRouter = require('./router/enfermeroRouter');
+
 app.use('/', homeRouter);
 app.use('/medico', medicoRouter);
 app.use('/recepcion', recepcionRouter);
 app.use('/enfermero', enfermeroRouter);
 
-
-app.use((req, res)=>{
-  res.status(404).render('error')
+app.use((req, res) => {
+  res.status(404).render('error');
 });
 
-
-
+// Definir el puerto desde la variable de entorno
 app.set("port", process.env.PORT || 3030);
 
-module.exports = app;
-
+module.exports = { app, sequelize }; // Exportamos sequelize también
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const app = require('./app');
+const { app, sequelize } = require('./app');
 
 const port = app.get('port');
-
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto: ${port}`);
-});
+sequelize.sync({ alter: true }) 
+  .then(() => {
+    console.log('Modelos sincronizados con la base de datos');
+    app.listen(port, () => {
+      console.log(`Servidor corriendo en puerto: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error al sincronizar modelos:', err);
+  });
