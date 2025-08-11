9ac6a28bb58befc6a97380b9519e9b5f4d1275e4
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-08-11 14:48:11 -0300

    Agregar modelo Persona: definir atributos y configuración para la tabla de personas en la base de datos

---
+++ model/Persona.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+class Persona extends Model { }
+
+Persona.init(
+  {
+    dni: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    apellido: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    genero: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fechaNacimiento: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      get() {
+        const raw = this.getDataValue('fechaNacimiento');
+        return raw
+          ? raw.toISOString().slice(0, 10)    // "YYYY-MM-DD"
+          : null;
+      }
+    },
+    idNacionalidad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'nacionalidad',
+        key: 'id'
+      }
+    },
+    domicilio: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    telefono: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+  sequelize,
+  modelName: "Persona",
+  tableName: "personas",
+}
+);
+
+module.exports = Persona;
