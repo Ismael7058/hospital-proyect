d9759dda59524594f39883afd7be5acd3972e53f
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2025-08-11 14:48:30 -0300

    Eliminar atributos innecesarios del modelo Paciente y ajustar referencia a la tabla persona

--- model/Paciente.js
+++ model/Paciente.js
@@ -5,52 +5,13 @@ class Paciente extends Model { }
 
 Paciente.init(
   {
-    dni: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true
-    },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    apellido: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    genero: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    fechaNacimiento: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      get() {
-        const raw = this.getDataValue('fechaNacimiento');
-        return raw
-          ? raw.toISOString().slice(0, 10)    // "YYYY-MM-DD"
-          : null;
-      }
-    },
-    idNacionalidad: {
+    idPersona: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'nacionalidad',
+        model: 'persona',
         key: 'id'
       }
-    },
-    domicilio: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    telefono: {
-      type: DataTypes.INTEGER,
-      allowNull: true
     }
   }, {
   sequelize,
